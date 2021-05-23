5055d1e8b60b7f514a7f0293ee137ce9848d48bf
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-23 16:49:15 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::env;
-use std::net::{Ipv6Addr, SocketAddrV6, ToSocketAddrs};
+use std::error::Error;
+use std::io;
+use std::net::{IpAddr, Ipv6Addr, SocketAddr, TcpStream, ToSocketAddrs};
+use std::process;
+use std::thread;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use clap::{App, AppSettings, Arg, SubCommand};
@@ -83,6 +87,15 @@ fn main() {
     }
 }
 
+fn build_replaced_ip<S: Into<String>>(secret: S, ip: S) -> Result<Ipv6Addr, Box<dyn Error>> {
+    let auth = otpauth::TOTP::from_base32(secret).unwrap(); // TODO: fix error handling
+    let ts = SystemTime::now().duration_since(UNIX_EPOCH)?;
+    let code = auth.generate(30, ts.as_secs());
+
+    let raw_ip = build_ip(&ip.into(), code)?;
+    Ok(raw_ip.parse().unwrap()) // TODO: fix error handling
+}
+
 fn generate_ip() {
     let ip_template = match env::var("TOSH_IP_TEMPLATE") {
         Ok(val) => val,
@@ -100,21 +113,87 @@ fn generate_ip() {
         ),
     };
 
-    let auth = otpauth::TOTP::from_base32(totp_secret).unwrap();
-    let ts = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
-    let code = auth.generate(30, ts.as_secs());
-
-    let raw_ip = build_ip(&ip_template, code).unwrap();
-    let ip: Ipv6Addr = raw_ip.parse().unwrap();
-
+    let ip = build_replaced_ip(totp_secret, ip_template).unwrap();
     println!("{}", ip);
 }
 
-fn connect(host: &str, port: u16) {
-    println!("connecting to {}:{}", host, port);
-
-    let available_addrs = format!("{}:{}", host, port).to_socket_addrs().unwrap();
-    for any_addr in available_addrs {
-        println!("{:?}", any_addr);
+fn fmt_ipv6_expanded(addr: &Ipv6Addr) -> String {
+    let mut built = String::default();
+    for (idx, segment) in addr.segments().iter().enumerate() {
+        let formatted = match idx {
+            6 => format!(
+                "{}xx",
+                format!("{:04x}", segment)
+                    .chars()
+                    .take(2)
+                    .collect::<String>()
+            ), // XXX: lazy and ugly!
+            7 => "xxxx".to_string(),
+            _ => format!("{:04x}", segment),
+        };
+        built.push_str(formatted.as_str());
+        if idx < 7 {
+            built.push(':');
+        }
     }
+    built
+}
+
+fn connect(host: &str, port: u16) {
+    let totp_secret = match env::var("TOSH_TOTP_SECRET") {
+        Ok(val) => val,
+        Err(err) => panic!(
+            "failed to read environment variable TOSH_TOTP_SECRET: {}",
+            err
+        ),
+    };
+
+    let available_addrs: Vec<SocketAddr> = format!("{}:{}", host, port)
+        .to_socket_addrs()
+        .unwrap()
+        .filter(|addr| addr.is_ipv6())
+        .collect();
+
+    if available_addrs.is_empty() {
+        eprintln!("[tosh] could not resolve any ipv6 addresses for {}", host);
+        process::exit(1);
+    }
+
+    // Pick first address, turn it into a template and then replace TOTP into it
+    let orig_ip = available_addrs[0].ip();
+    let ip_template = match orig_ip {
+        IpAddr::V6(ip) => fmt_ipv6_expanded(&ip),
+        _ => unreachable!(),
+    };
+
+    let ip = build_replaced_ip(&totp_secret, &ip_template).unwrap();
+    eprintln!("[tosh] resolved {} ({}) -> {}", orig_ip, ip_template, &ip);
+
+    // Build new socket address
+    let target_addr = SocketAddr::from((ip, port));
+    if let Err(err) = do_proxy(target_addr) {
+        eprintln!("[tosh] failed to proxy: {}", err);
+    }
+
+    process::exit(1);
+}
+
+fn do_proxy(addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    let mut conn = TcpStream::connect(addr)?;
+    let mut conn_w = conn.try_clone()?;
+
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let reader = thread::spawn(move || {
+        io::copy(&mut conn, &mut stdout).unwrap();
+    });
+    let writer = thread::spawn(move || {
+        io::copy(&mut stdin, &mut conn_w).unwrap();
+    });
+
+    reader.join().unwrap(); // TODO: fix error handling
+    writer.join().unwrap(); // TODO: fix error handling
+
+    Ok(())
 }
